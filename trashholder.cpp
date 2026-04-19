90217f9b84015511ccaa27bd56eb2396f7d7477a
CleitonBR4 <120283537+CleitonBR4@users.noreply.github.com>
2026-04-19 13:32:13 -0300

    upload files

---
+++ trashholder.cpp
@@ -0,0 +1,51 @@
+////////////////////////////////////////////////////////////////////////
+// OpenTibia - an opensource roleplaying game
+////////////////////////////////////////////////////////////////////////
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+////////////////////////////////////////////////////////////////////////
+#include "otpch.h"
+#include "trashholder.h"
+
+#include "game.h"
+#include "spells.h"
+#include "const.h"
+
+extern Game g_game;
+
+void TrashHolder::__addThing(Creature* actor, int32_t, Thing* thing)
+{
+	if(Item* item = thing->getItem())
+	{
+		if(item == this || !item->isMovable())
+			return;
+
+		if(g_game.isSwimmingPool(this, getTile(), true))
+		{
+			if(item->getID() == ITEM_WATERBALL_SPLASH)
+				return;
+
+			if(item->getID() == ITEM_WATERBALL)
+			{
+				g_game.transformItem(item, ITEM_WATERBALL_SPLASH);
+				return;
+			}
+		}
+
+		g_game.internalRemoveItem(actor, item);
+		if(effect != MAGIC_EFFECT_NONE)
+			g_game.addMagicEffect(getPosition(), effect);
+	}
+	// --- SISTEMA DE NATAÇÃO REMOVIDO ---
+	// O bloco 'else if' que forçava o SWIMMING_OUTFIT no login foi apagado.
+}
